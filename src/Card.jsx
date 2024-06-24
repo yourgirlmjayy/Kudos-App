01349e18567fa54676f421000c929d1b94d04121
Mojolajesu Mercy Dada <mojolajesu@meta.com>
2024-06-24 14:31:01 -0700

    updated video

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,7 +2,7 @@ import './Card.css';
 
 import React from 'react';
 
-function Card({title, description, imgUrl, author, upvotes, handleDelete, key}) {
+function Card({title, description, imgUrl, author, upvotes, handleDelete, id}) {
   return (
     <div className="card">
         <h3>{title}</h3>
@@ -10,7 +10,8 @@ function Card({title, description, imgUrl, author, upvotes, handleDelete, key}) 
         <img src={imgUrl} alt="Card" />
         <p>{author}</p>
         <button>Upvote: {upvotes}</button>
-        <button onClick={()=> handleDelete(key)}>Delete</button>
+        <button onClick={() => handleDelete(id)}>Delete</button>
+        
     </div>
   );}
 
