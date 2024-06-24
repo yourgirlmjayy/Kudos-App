e528610cf234dc05c48f3c4d0bb71860e2a5d8de
Mojolajesu Mercy Dada <mojolajesu@meta.com>
2024-06-23 20:14:00 -0700

    created url field that autoupdates when gif is clicked

--- src/CreateCardForm.jsx
+++ src/CreateCardForm.jsx
@@ -1,17 +1,32 @@
 import './CreateCardForm.css'
 import { useState } from 'react';
-function CreateCardForm ({closeModal, onModalDataChange, boardData, submitForm}) {
+function CreateCardForm ({closeModal, boardId, submitForm}) {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
     const [searchInput, setSearchInput] = useState("");
     const [gifs, setgifs] = useState([]);
     const [gifsURL, setGifsURL] = useState("");
+    const [author, setAuthor] = useState('');
 
     const handleSearchSubmit = () => {
         fetchGiphy();
       }
 
+    const handleSubmit = () => {
+        const cardData = {
+            title,
+            description,
+            gifsURL,
+            author,
+            boardId
+        };
+        submitForm(cardData);
+    };
+
     const fetchGiphy = async() => {
         const API_KEY = import.meta.env.VITE_GIFY_API_KEY
         let url;
+        // import gifs based on user's search input 
         url = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchInput}`;
     
         const options = {
@@ -29,14 +44,15 @@ function CreateCardForm ({closeModal, onModalDataChange, boardData, submitForm})
     }
 
     const handleInputChange = (event) => {
+        //update searchInput whenever user types in the search field
         event.preventDefault()
         setSearchInput(event.target.value)
         
     }
 
     const handleSetGifsUrl = (url) => {
-        setGifsURL(url.target.value);
-    }
+        setGifsURL(url);
+      };   
 
     return (
         <div className="modal-overlay">
@@ -44,24 +60,26 @@ function CreateCardForm ({closeModal, onModalDataChange, boardData, submitForm})
                 <button className="close-button" onClick={closeModal}>&times;</button>
                 <h2>Create a New Card</h2>
                 <p>Title:</p>
-                <input name="title" type="text" placeholder="Title..."/>
+                <input className="title" type="text" value={title} onChange={(event) => setTitle(event.target.value)} placeholder="Title..."/>
                 <p>Description:</p>
-                <input name="description" type="text" placeholder="Description..."/>
-                <p>GIF:</p>
-                <input name="searchGIFs" type="text" value={searchInput} onChange={handleInputChange} placeholder="Search..."/>
+                <input className="description" type="text" value={description} onChange={(event) => setDescription(event.target.value)} placeholder="Description..."/>
+                <p>Search GIFs:</p>
+                <input className="searchGIFs" type="text" value={searchInput} onChange={handleInputChange} placeholder="Search..."/>
                 <button onClick={handleSearchSubmit}>Search</button>
-                <p>URL:</p>
-                <input className='gifs-url' type="text" value={gifsURL}></input>
                 <div className='giffy-images'>  
                     {gifs.map((gif) => (
                         <img className='gifs'
                             src={gif.images.downsized.url}
-                            onClick={handleSetGifsUrl}
+                            onClick={() => handleSetGifsUrl(gif.images.downsized.url)}
+                            alt = {gif.alt_text}
                         />
                     ))}
                 </div>
+                <p>GIF Url:</p>
+                <input className='gifs-url' type="text" value={gifsURL} onChange={(event) => setGifsURL(event.target.value)}></input>
                 <p>Author:</p>
-                <input name='author' type="text"  placeholder="Optional"/>
+                <input className='author' type="text" value={author} onChange={(event) => setAuthor(event.target.value)} placeholder="Optional"/>
+                <p></p>
                 <button onClick={submitForm}>Create Card</button> 
                 {/* //create submit form function */}
             </div>
--- src/Header.jsx
+++ src/Header.jsx
@@ -4,8 +4,7 @@ import appLogo from './Star-img.webp'
 function Header() {
     return (
         <div className='app-header'>
-                <img className='app-logo' src={appLogo} alt='appLogo'/>
-                <h1 className='app-name'>KUDOS BOARD</h1>
+                <h1 className='app-name'>🎆KUDOS BOARD🎆</h1>
         </div>
     )
 }
